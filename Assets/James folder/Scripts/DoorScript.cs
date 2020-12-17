13465c75ac387aa68c3c6bf7dd6e25f84c555cd5
PAPAPAPAYA <dameningen@126.com>
2020-12-17 23:51:13 +0800

    + door but not really

---
+++ Assets/James folder/Scripts/DoorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Transform aiTarget;
+	public GameObject ghost;
+	public float durability;
+	private float actualDurability;
+	public bool closed;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Ghost"))
+		{
+			print("ghost on door");
+			aiTarget.position = ghost.transform.position;
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.CompareTag("Ghost"))
+		{
+			actualDurability -= 1f * Time.deltaTime;
+		}
+	}
+
+	private void Start()
+	{
+		actualDurability = durability;
+	}
+
+	private void Update()
+	{
+		if (actualDurability <= 0)
+		{
+			actualDurability = durability;
+			closed = false;
+		}
+	}
+}
