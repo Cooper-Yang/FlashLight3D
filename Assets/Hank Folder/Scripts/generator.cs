79759b6205931153062dc6757a8c33da893885bc
Hankf007 <hankf007@gmail.com>
2020-12-19 13:02:17 -0500

    generator

---
+++ Assets/Hank Folder/Scripts/generator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+    public int fuelAmt;
+    public int fuelCap;
+
+    public GameObject player;
+    public int invFuel;
+
+    public bool canWork;
+    void Start()
+    {
+        fuelCap = 5;
+        canWork = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        invFuel = player.GetComponent<inventory>().fuelCarried;
+
+        if (Input.GetKeyDown(KeyCode.F))//pur fuel into generator
+        {
+            fuelAmt += invFuel;
+            player.GetComponent<inventory>().fuelCarried=0;
+            
+        }
+
+        if (fuelAmt == fuelCap)//gas is full
+        {
+            canWork = true;
+        }
+    }
+}
---
+++ Assets/Hank Folder/Scripts/inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public int fuelCarried;
+
+
+    void Start()
+    {
+        fuelCarried = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) //pick up fuel
+        {
+            fuelCarried++;
+            
+        }
+    }
+}
