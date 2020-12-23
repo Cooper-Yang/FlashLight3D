8f1027803c478e0745be2053f1c0e778a7c89b58
Cooper Yang <ty1037@nyu.edu>
2020-12-22 20:12:05 -0500

    Add hank stuff Key interaction

---
+++ Assets/Hank Folder-20201222T225510Z-001/Hank Folder/Scripts/generator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class generator : MonoBehaviour
+{
+    public int fuelAmt;
+    public int fuelCap;
+
+    public int invFuel;
+
+    
+
+    public Text genFuel;
+
+    void Start()
+    {
+        fuelCap = 5;
+        inventory.Instance.generatorCanWork = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        invFuel = inventory.Instance.fuelCarried;
+
+        genFuel.text = fuelAmt + " / 5";
+
+        if (fuelAmt == fuelCap)//gas is full
+        {
+            inventory.Instance.generatorCanWork = true;
+        }
+    }
+
+    public void AddFuel()
+    {
+        fuelAmt += invFuel;
+        inventory.Instance.fuelCarried = 0;
+    }
+}
