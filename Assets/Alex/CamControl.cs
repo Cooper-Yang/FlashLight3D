c9e06e5a25635214d1f7521ab8382778df800481
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2020-12-26 19:23:18 +0800

    Dead Animation

--- Assets/Alex/CamControl.cs
+++ Assets/Alex/CamControl.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CamControl : MonoBehaviour
 {
-    public Camera deathCam;
-    public Camera mainCam;
-    public bool isDead;
-    public GameObject black;
-
+    public Camera deathCam;//new cam called deeathcam
+    public Camera mainCam;//player cam
+    public bool isDead;//dead bool
+    public GameObject black;//black panel
+    public GameObject player;
+    [Header("Controll Times")]
+    //No need to change non-def floats. Public is easy for checking.
+    //Change only def floats.
     public float defFallSpeed;
     public float fallSpeed;
     public float defFallTime;
     public float fallTime;
+    public float defRotateTime;
+    public float rotateTime;
 
     void Start()
     {
@@ -21,6 +27,7 @@ public class CamControl : MonoBehaviour
         mainCam.gameObject.SetActive(true);
         fallSpeed = defFallSpeed;
         fallTime = defFallTime;
+        rotateTime = defRotateTime;
     }
 
     void Update()
@@ -32,16 +39,28 @@ public class CamControl : MonoBehaviour
             deathCam.gameObject.SetActive(true);
             mainCam.gameObject.SetActive(false);
             if (fallSpeed > 0)
+            {
                 fallSpeed -= 0.8f * Time.deltaTime;
-            deathCam.transform.position = new Vector3(0, Mathf.Lerp(deathCam.transform.position.y, 1f, 1 - fallSpeed/defFallSpeed), 0);
+                //fallSpeed *= 1.01f;
+            }
+            deathCam.transform.position = new Vector3(deathCam.transform.position.x, Mathf.Lerp(deathCam.transform.position.y, 1f, 1 - fallSpeed/defFallSpeed), deathCam.transform.position.z);
             if(fallSpeed <= 0)
             {
                 if(fallTime <= 0)
                 {
                     fallTime = 0f;
                     if (fallSpeed > -defFallSpeed)
+                    {
                         fallSpeed -= 0.8f * Time.deltaTime;
-                    deathCam.transform.position = new Vector3(0, Mathf.Lerp(deathCam.transform.position.y, 0.2f, (fallSpeed / defFallSpeed) * -1), 0);
+                        fallSpeed *= 1.01f;
+                    }
+                    rotateTime -= 0.8f * Time.deltaTime;
+                    if (rotateTime < 0)
+                        rotateTime = 0;
+                    deathCam.transform.position = new Vector3(deathCam.transform.position.x, 
+                                                              Mathf.Lerp(deathCam.transform.position.y, 0.45f, (fallSpeed / defFallSpeed) * -1), deathCam.transform.position.z);
+                    deathCam.transform.rotation = Quaternion.Euler(deathCam.transform.rotation.eulerAngles.x, deathCam.transform.rotation.eulerAngles.y, 
+                                                  Mathf.Lerp(deathCam.transform.rotation.eulerAngles.z, -76f, 1 - rotateTime/defRotateTime));
                     Color color = black.GetComponent<Image>().color;
                     color.a += 0.8f * Time.deltaTime;
                     black.GetComponent<Image>().color = new Color(0, 0, 0, color.a);
@@ -49,6 +68,13 @@ public class CamControl : MonoBehaviour
                 else if(fallTime > 0)
                 {
                     fallTime -= 0.8f * Time.deltaTime;
+                }
+            }
+            if(black.GetComponent<Image>().color.a >= 1)
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    SceneManager.LoadScene(0);
                 }
             }
         }
