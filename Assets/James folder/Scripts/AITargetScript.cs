77de26044ccbfedac591eba6c5ab35dbdb55d230
PAPAPAPAYA <dameningen@126.com>
2020-12-26 15:47:22 +0800

    + toggle ai chase

--- Assets/James folder/Scripts/AITargetScript.cs
+++ Assets/James folder/Scripts/AITargetScript.cs
@@ -47,4 +47,14 @@ public class AITargetScript : MonoBehaviour
 			}
 		}
 	}
+	
+	public void ChasePlayer(Transform playerTransform)
+	{
+		playerLightSource = playerTransform;
+	}
+
+	public void StopChasingPlayer()
+	{
+		playerLightSource = null;
+	}
 }
